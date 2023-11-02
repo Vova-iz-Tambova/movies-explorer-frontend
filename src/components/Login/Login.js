218478b13bec8e7529a71a030e553de73d5e7466
Vova iz Tambova <voller.ruden@yandex.ru>
2023-11-02 20:43:05 +0300

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,28 @@
+import './Login.css';
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+
 function Login() {
   return (
-    <>
-    </>
+    <section className='login'>
+      <NavLink to="/" className="login__logo">
+        <img src={logo} alt="логотип" /></NavLink>
+      <h2 className='login__title'>Рады видеть!</h2>
+      <p className='login__description'>E-mail</p>
+      <div className='login__field'>
+        <div className='login__input'>pochta@yandex.ru|</div>
+      </div>
+      <p className='login__error'>Что-то пошло не так...</p>
+      <p className='login__description'>Пароль</p>
+      <div className='login__field'>
+        <div className='login__input'></div>
+      </div>
+      <p className='login__error'>Что-то пошло не так...</p>
+      <div className='login__submit'>Войти</div>
+      <div className='login__nav'>
+        <p >Ещё не зарегистрированы? <NavLink to="/signup">Регистрация</NavLink></p>
+      </div>
+    </section>
   )
 }
 export default Login;
