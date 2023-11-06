08091ca5fcb0174802313caadc803bb9bc1099c5
Vova iz Tambova <voller.ruden@yandex.ru>
2023-11-06 10:11:13 +0300

    fix portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -5,15 +5,15 @@ function Portfolio() {
   return (
     <section className='portfolio'>
       <h3 className='portfolio__title'>Портфолио</h3>
-      <Link to='https://vova-iz-tambova.github.io/how-to-learn/' target='_blank' className='portfolio__navlink__effect'>
+      <Link to='https://vova-iz-tambova.github.io/how-to-learn/' target='_blank' className='portfolio__nav  link__effect'>
         <p className='portfolio__name'>Статичный сайт</p>
         <p className='portfolio__arrow'>↗</p>
       </Link>
-      <Link to='https://vova-iz-tambova.github.io/russian-travel/' target='_blank' className='portfolio__navlink__effect'>
+      <Link to='https://vova-iz-tambova.github.io/russian-travel/' target='_blank' className='portfolio__nav  link__effect'>
         <p className='portfolio__name'>Адаптивный сайт</p>
         <p className='portfolio__arrow'>↗</p>
       </Link>
-      <Link to='https://vova-iz-tambova.github.io/mesto-react/' target='_blank' className='portfolio__navlink__effect'>
+      <Link to='https://vova-iz-tambova.github.io/mesto-react/' target='_blank' className='portfolio__nav  link__effect'>
         <p className='portfolio__name'>Одностраничное приложение</p>
         <p className='portfolio__arrow'>↗</p>
       </Link>
