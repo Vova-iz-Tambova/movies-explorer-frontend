1e6f6a3ae4ec4b054498676633254d7fb49e75be
Vova iz Tambova <voller.ruden@yandex.ru>
2023-11-09 06:18:55 +0300

    fix NavTab

--- src/components/Main/NavTab/NavTab.js
+++ src/components/Main/NavTab/NavTab.js
@@ -2,15 +2,19 @@ import './NavTab.css';
 
 function NavTab() {
   return (
-    <>
-      <nav className='nav-tab'>
-        <ul className='nav-tab__links'>
-          <li><h2><a href='#project' className='nav-tab__link  effect'>О проекте</a></h2></li>
-          <li><h2><a href='#techs' className='nav-tab__link  effect'>Технологии</a></h2></li>
-          <li><h2><a href='#me' className='nav-tab__link  effect'>Студент</a></h2></li>
-        </ul>
-      </nav>
-    </>
+    <nav className='nav-tab'>
+      <ul className='nav-tab__links'>
+        <li>
+          <a href='#project' className='nav-tab__link  effect'>О проекте</a>
+        </li>
+        <li>
+          <a href='#techs' className='nav-tab__link  effect'>Технологии</a>
+        </li>
+        <li>
+          <a href='#me' className='nav-tab__link  effect'>Студент</a>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
