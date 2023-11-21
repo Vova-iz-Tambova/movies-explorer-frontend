3b426b44d973375bddddedfc0bf67834385ccc8f
Vova iz Tambova <voller.ruden@yandex.ru>
2023-11-21 20:55:49 +0300

    rapair 2

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -6,86 +6,91 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 function MoviesCardList({ movies, savedMovies, onLikeMovie, onDeleteMovie }) {
   const [renderMovies, setRenderMovies] = useState([]);
   const [quantity, setQuantity] = useState(12);
-	const [showMoreFilms, setShowMoreFilms] = useState(3);
+  const [showMoreFilms, setShowMoreFilms] = useState(3);
 
   const { pathname } = useLocation();
 
-	useEffect(() => {
-		onResize()
-		window.addEventListener("resize", onResize)
+  useEffect(() => {
+    onResize()
+    window.addEventListener("resize", onResize)
 
-		return () => {
-			window.removeEventListener("resize", onResize)
-		}
-	}, []);
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
+  }, []);
 
   let timeout = null;
 
   function onResize() {
-		timeout && clearTimeout(timeout);
+    timeout && clearTimeout(timeout);
 
-		timeout = setTimeout(() => {
-			const width = window.innerWidth;
-			if (pathname === '/saved-movies') {
-				setRenderMovies(movies);
-			}
-			if (width > 1280) {
-				setQuantity(12);
-				setShowMoreFilms(3);
-			} if (width < 1279) {
-				setQuantity(8);
-				setShowMoreFilms(2);
-			} if (width < 768) {
-				setQuantity(5);
-				setShowMoreFilms(2);
-			}
-		}, 100);
-	}
+    timeout = setTimeout(() => {
+      const width = window.innerWidth;
+      if (pathname === '/saved-movies') {
+        setRenderMovies(movies);
+      } else {
+        if (width > 1280) {
+          setQuantity(12);
+          setShowMoreFilms(3);
+        } if (width < 1279) {
+          setQuantity(8);
+          setShowMoreFilms(2);
+        } if (width < 768) {
+          setQuantity(5);
+          setShowMoreFilms(2);
+        }
+      }
+    }, 100);
+  }
 
-	React.useEffect(() => {
-		setRenderMovies(movies.slice(0, quantity))
-	}, [movies, quantity]);
-
-	function handleMoreClick() {
-		const newArray = [
-			...movies.slice(0, (quantity + showMoreFilms))
-		]
-		setQuantity(quantity + showMoreFilms);
-		setRenderMovies(newArray);
-	}
-
-return (
-  <section>
-    {renderMovies.length > 0 ? (
-      <>
-        <ul className={`list ${(pathname === '/saved-movies') && `list__saved`}`}>
-          {renderMovies.map((movie) => (
-            <MoviesCard
-              key={movie.id || movie.movieId}
-              movie={movie}
-              savedMovies={savedMovies}
-              onLikeMovie={onLikeMovie}
-              onDeleteMovie={onDeleteMovie}
-            />
-          ))}
-        </ul>
-      </>
-    ) :
-      (
-        <p className='list__message'>Ничего не найдено</p>
-      )}
-
-    {renderMovies.length === movies.length ? '' :
-      <div className={pathname !== '/saved-movies' ? 'list__pagination' : 'list__pagination_disable'}>
-        <button
-          className='list__more  animation'
-          type='button'
-          onClick={handleMoreClick}
-        >Ещё</button>
-      </div>
+  React.useEffect(() => {
+    if (pathname === '/movies') {
+      setRenderMovies(movies.slice(0, quantity));
+    } else {
+      setRenderMovies(movies);
     }
-  </section >
-);
+  }, [movies, quantity]);
+
+  function handleMoreClick() {
+    const newArray = [
+      ...movies.slice(0, (quantity + showMoreFilms))
+    ]
+    setQuantity(quantity + showMoreFilms);
+    setRenderMovies(newArray);
+  }
+
+  return (
+    <section>
+      {renderMovies.length > 0 ? (
+        <>
+          <ul className={`list ${(pathname === '/saved-movies') && `list__saved`}`}>
+            {renderMovies.map((movie) => (
+              <MoviesCard
+                key={movie.id || movie.movieId}
+                movie={movie}
+                savedMovies={savedMovies}
+                onLikeMovie={onLikeMovie}
+                onDeleteMovie={onDeleteMovie}
+              />
+            ))}
+          </ul>
+        </>
+      ) :
+        (
+          <p className='list__message'>Ничего не найдено</p>
+        )}
+
+      {renderMovies.length === movies.length ? '' :
+        <div className={pathname !== '/saved-movies' ? 'list__pagination' : 'list__pagination_disable'}>
+          <button
+            className='list__more  animation'
+            type='button'
+            onClick={handleMoreClick}
+          >Ещё</button>
+        </div>
+      }
+    </section >
+  );
 };
 
 export default MoviesCardList;
